6eee82e1ebd2f76e218bbdd1a60e20a40bb50843
Елизавета Арбуз <96349596+ovivvvu@users.noreply.github.com>
2022-11-27 23:40:38 +0200

    УХ МАРК

---
+++ vzlom_doti.py
@@ -0,0 +1,13 @@
+import time
+
+print("наскока сильна взламат жопа(дота):")
+
+vzlom_count = int(input())
+i = 0
+
+while i < vzlom_count+1: 
+    time.sleep(0.5)
+    print(f"взлом жопи {i}/{vzlom_count}")
+    i+=1
+
+print("ДОТА ВЗЛОМАНА!!! ОТЧИМЫ УШЛИ!!!!!!!!!!!!!!!1")
